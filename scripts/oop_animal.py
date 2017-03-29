6785f820d5062159df7fe1bac3198b01fb943df4
liuz <liuzhuan@qingsongchou.com>
2017-03-29 18:25:41 +0800

    Add oop basics knowledge

---
+++ scripts/oop_animal.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+class Animal(object):
+    def run(self):
+        print('Animal is running...')
+
+class Dog(Animal):
+    def run(self):
+        print('Dog is running....')
+    def eat(self):
+        print('Eating meat...')
+
+class Cat(Animal):
+    def run(self):
+        print('Cat is running....')
+
+dog = Dog()
+dog.run()
+
+cat = Cat()
+cat.run()
+
+def run_twice(animal):
+    print('*** twice begin ***')
+    animal.run()
+    animal.run()
+    print('*** twice end ***')
+
+run_twice(dog)
